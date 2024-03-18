857156643d97b277b5cf2b43be0af0db2c07fb5e
Dilmurod772 <karimovd892@gmail.com>
2024-03-19 00:29:43 +0500

    Contollers were added

---
+++ WAD.00014333/Controllers/CompaniesController.cs
@@ -0,0 +1,76 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using WAD._00014333.DTOs;
+using WAD._00014333.Interfaces;
+using WAD._00014333.Models;
+
+namespace WAD._00014333.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompaniesController : ControllerBase
+    {
+        private readonly ICompanyRepository _companyRepository;
+        private readonly IMapper _mapper;
+
+        public CompaniesController(ICompanyRepository companyRepository, IMapper mapper)
+        {
+            _companyRepository = companyRepository;
+            _mapper = mapper;
+        }
+
+        // GET: api/Companies
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CompanyDto>>> GetCompanies()
+        {
+            var companies = await _companyRepository.GetAllCompaniesAsync();
+            return Ok(_mapper.Map<IEnumerable<CompanyDto>>(companies));
+        }
+
+        // GET: api/Companies/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CompanyDto>> GetCompany(int id)
+        {
+            var company = await _companyRepository.GetCompanyByIdAsync(id);
+
+            if (company == null)
+            {
+                return NotFound();
+            }
+
+            return _mapper.Map<CompanyDto>(company);
+        }
+
+        // POST: api/Companies
+        [HttpPost]
+        public async Task<ActionResult<CompanyDto>> PostCompany(CompanyDto companyDto)
+        {
+            var company = _mapper.Map<Company>(companyDto);
+            company = await _companyRepository.AddCompanyAsync(company);
+            return CreatedAtAction(nameof(GetCompany), new { id = company.CompanyId }, _mapper.Map<CompanyDto>(company));
+        }
+
+        // PUT: api/Companies/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCompany(int id, CompanyDto companyDto)
+        {
+            if (id != companyDto.CompanyId)
+            {
+                return BadRequest();
+            }
+
+            var company = _mapper.Map<Company>(companyDto);
+            await _companyRepository.UpdateCompanyAsync(company);
+
+            return NoContent();
+        }
+
+        // DELETE: api/Companies/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCompany(int id)
+        {
+            await _companyRepository.DeleteCompanyAsync(id);
+            return NoContent();
+        }
+    }
+}
---
+++ WAD.00014333/Controllers/JobsController.cs
@@ -0,0 +1,76 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using WAD._00014333.DTOs;
+using WAD._00014333.Interfaces;
+using WAD._00014333.Models;
+
+namespace WAD._00014333.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobsController : ControllerBase
+    {
+        private readonly IJobRepository _jobRepository;
+        private readonly IMapper _mapper;
+
+        public JobsController(IJobRepository jobRepository, IMapper mapper)
+        {
+            _jobRepository = jobRepository;
+            _mapper = mapper;
+        }
+
+        // GET: api/Jobs
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<JobDto>>> GetJobs()
+        {
+            var jobs = await _jobRepository.GetAllJobsAsync();
+            return Ok(_mapper.Map<IEnumerable<JobDto>>(jobs));
+        }
+
+        // GET: api/Jobs/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<JobDto>> GetJob(int id)
+        {
+            var job = await _jobRepository.GetJobByIdAsync(id);
+
+            if (job == null)
+            {
+                return NotFound();
+            }
+
+            return _mapper.Map<JobDto>(job);
+        }
+
+        // POST: api/Jobs
+        [HttpPost]
+        public async Task<ActionResult<JobDto>> PostJob(JobDto jobDto)
+        {
+            var job = _mapper.Map<Job>(jobDto);
+            job = await _jobRepository.AddJobAsync(job);
+            return CreatedAtAction(nameof(GetJob), new { id = job.JobId }, _mapper.Map<JobDto>(job));
+        }
+
+        // PUT: api/Jobs/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutJob(int id, JobDto jobDto)
+        {
+            if (id != jobDto.JobId)
+            {
+                return BadRequest();
+            }
+
+            var job = _mapper.Map<Job>(jobDto);
+            await _jobRepository.UpdateJobAsync(job);
+
+            return NoContent();
+        }
+
+        // DELETE: api/Jobs/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteJob(int id)
+        {
+            await _jobRepository.DeleteJobAsync(id);
+            return NoContent();
+        }
+    }
+}
