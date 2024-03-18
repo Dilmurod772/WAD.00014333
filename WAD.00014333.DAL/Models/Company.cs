ddfa7bcc722425fcc1c99ec5eceb7a49443688c3
Dilmurod772 <karimovd892@gmail.com>
2024-03-19 00:27:26 +0500

    Added Models

---
+++ WAD.00014333.DAL/Models/Company.cs
@@ -0,0 +1,11 @@
+﻿namespace WAD._00014333.Models
+{
+    public class Company
+    {
+        public int CompanyId { get; set; }
+        public string Name { get; set; }
+        public string Industry { get; set; }
+        public string Location { get; set; } 
+        public ICollection<Job> Jobs { get; set; } 
+    }
+}
---
+++ WAD.00014333.DAL/Models/Job.cs
@@ -0,0 +1,13 @@
+﻿namespace WAD._00014333.Models
+{
+    public class Job
+    {
+        public int JobId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Requirements { get; set; } 
+        public DateTime PostedDate { get; set; }
+        public int CompanyId { get; set; } 
+        public Company Company { get; set; }
+    }
+}
